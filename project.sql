f509d8fa7872e7096c5d4f90fff56a68c12fb42c
Jackiin <38468577+Jackiin@users.noreply.github.com>
2018-04-23 16:24:46 +0800

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2018 at 11:47 AM
+-- Generation Time: Apr 23, 2018 at 10:24 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -29,19 +29,75 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `employee` (
-  `id` varchar(255) NOT NULL,
-  `fname` varchar(255) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `salary` int(255) NOT NULL,
-  `tel` int(255) NOT NULL
+  `id` varchar(10) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `salary` int(12) NOT NULL,
+  `tel` text NOT NULL,
+  `dismissed` tinyint(1) NOT NULL,
+  `position` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `employee`
+-- Table structure for table `food`
 --
 
-INSERT INTO `employee` (`id`, `fname`, `lname`, `salary`, `tel`) VALUES
-('001', 'Fong Fong', 'Chow', 12000, 29488888);
+CREATE TABLE `food` (
+  `id` varchar(3) NOT NULL,
+  `zone_id` varchar(1) NOT NULL,
+  `name` text NOT NULL,
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` varchar(12) NOT NULL,
+  `name` text NOT NULL,
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_item`
+--
+
+CREATE TABLE `manage_item` (
+  `employee_id` varchar(10) NOT NULL,
+  `item_id` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` varchar(12) NOT NULL,
+  `employee_id` varchar(10) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_record`
+--
+
+CREATE TABLE `orders_record` (
+  `food_id` varchar(3) NOT NULL,
+  `order_id` varchar(12) NOT NULL,
+  `quantity` int(3) NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,6 +117,26 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`name`, `password`) VALUES
 ('Jack', 311520);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone`
+--
+
+CREATE TABLE `zone` (
+  `id` varchar(1) NOT NULL,
+  `name` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `zone`
+--
+
+INSERT INTO `zone` (`id`, `name`) VALUES
+('A', 'BBQ - 燒味'),
+('B', 'Chinese Food - 中餐'),
+('C', 'Western Food - 西餐');
+
 --
 -- Indexes for dumped tables
 --
@@ -72,10 +148,57 @@ ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manage_item`
+--
+ALTER TABLE `manage_item`
+  ADD PRIMARY KEY (`employee_id`,`item_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `orders_record`
+--
+ALTER TABLE `orders_record`
+  ADD PRIMARY KEY (`food_id`,`order_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `zone`
+--
+ALTER TABLE `zone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
