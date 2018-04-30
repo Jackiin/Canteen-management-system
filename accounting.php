5bf7fb76ccf18a732774ede34c85c0a79bab1717
Jackiin <38468577+Jackiin@users.noreply.github.com>
2018-04-30 16:19:25 +0800

    Add files via upload

---
+++ accounting.php
@@ -0,0 +1,123 @@
+<!--This page is for customer-->
+<!DOCTYPE html> 
+
+<head>
+
+	<title>Accounting</title>
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="stylesheet" href="/css/order.css">
+	<link rel="stylesheet" href="/css/table.css">
+	<script src="/js/jquery-3.3.1.min.js"></script>
+	<script src="/js/submit_order.js"></script>	
+	
+</head>
+
+<body>
+
+	<div class="center">
+		<img src="/image/ap.png" style="width:10%;height:10%" />
+	</div>
+	
+	<?php
+	function show($dish01, $quantity01, $dish02, $quantity02, $dish03, $quantity03, $dish04, $quantity04) {
+		
+		include($_SERVER['DOCUMENT_ROOT'].'/connect.php');
+		
+		$q1 = intval($quantity01);
+		$q2 = intval($quantity02);
+		$q3 = intval($quantity03);
+		$q4 = intval($quantity04);
+		
+		$n1 = "";
+		$n2 = "";
+		$n3 = "";
+		$n4 = "";
+		
+		$p1 = 0;
+		$p2 = 0;
+		$p3 = 0;
+		$p4 = 0;
+		
+		$unitp = array(0, 0, 0, 0);
+		
+		$sql1 = "SELECT `price`, `name` from food WHERE `id` = '$dish01'";
+		$result1 = mysqli_query($conn, $sql1);
+		$row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC);
+		$unitp[0] = $row1['price'];
+		$p1 = $row1['price'] * $q1;
+		$n1 = $row1['name'];
+		$sql2 = "SELECT `price`, `name` from food WHERE `id` = '$dish02'";
+		$result2 = mysqli_query($conn, $sql2);
+		$row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+		$unitp[1] = $row2['price'];
+		$p2 = $row2['price'] * $q2;
+		$n2 = $row2['name'];
+		$sql3 = "SELECT `price`, `name` from food WHERE `id` = '$dish03'";
+		$result3 = mysqli_query($conn, $sql3);
+		$row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC);
+		$unitp[2] = $row3['price'];
+		$p3 = $row3['price'] * $q3;
+		$n3 = $row3['name'];
+		$sql4 = "SELECT `price`, `name` from food WHERE `id` = '$dish04'";
+		$result4 = mysqli_query($conn, $sql4);
+		$row4 = mysqli_fetch_array($result4, MYSQLI_ASSOC);
+		$unitp[3] = $row4['price'];
+		$p4 = $row4['price'] * $q4;
+		$n4 = $row4['name'];
+		
+		session_start();
+		$ids = array($dish01, $dish02, $dish03, $dish04);
+		$names = array($n1, $n2, $n3, $n4);
+		$quantities = array($q1, $q2, $q3, $q4);
+		$prices = array($p1, $p2, $p3, $p4);
+		$_SESSION['ids'] = $ids;
+		$_SESSION['names'] = $names;
+		$_SESSION['unitprices'] = $unitp;
+		$_SESSION['prices'] = $prices;		
+		$_SESSION['quantities'] = $quantities;
+		
+		echo '
+			<table style="margin:auto;width:80%">
+			<thead>
+			<tr>
+				<th align="center" style="width:800px">Dish</th>
+				<th align="center" style="width:200px">Quantity</th>
+				<th align="center">Price</th>
+			</tr>
+			</thead>
+			<tbody>
+			';
+		
+		echo '<tr><td align="center" />'.$n1.'<td align="center" />'.$q1.'<td align="center" />'.$p1.'</tr>'.
+			 '<td align="center" />'.$n2.'<td align="center" />'.$q2.'<td align="center" />'.$p2.'</tr>'.
+			 '<td align="center" />'.$n3.'<td align="center" />'.$q3.'<td align="center" />'.$p3.'</tr>'.
+			 '<td align="center" />'.$n4.'<td align="center" />'.$q4.'<td align="center" />'.$p4.'</tr>'.
+			 '<td align="center" />'.'<b>Total</b>'.'<td align="center" />'.($q1+$q2+$q3+$q4).'<td align="center" />'.($p1+$p2+$p3+$p4).'</tr>';
+			 
+		echo '</tbody></table>';
+	}
+
+	
+	$dish01 = $_GET['dish01'];
+	$quantity01 = $_GET['quantity01'];
+	$dish02 = $_GET['dish02'];
+	$quantity02 = $_GET['quantity02'];
+	$dish03 = $_GET['dish03'];
+	$quantity03 = $_GET['quantity03'];
+	$dish04 = $_GET['dish04'];
+	$quantity04 = $_GET['quantity04'];
+	
+
+	show($dish01, $quantity01, $dish02, $quantity02, $dish03, $quantity03, $dish04, $quantity04);
+		
+	?>
+	
+	<div style="margin-top:20px;margin-right:150px" align="right">
+		<button id="next" onclick="window.location.href='/order.php'">Return</button>
+		<button id="order" onclick="window.location.href='/functions/end_order.php'">Order</button>
+	</div>
+
+</body>
+
+
+</html>
---
+++ staff.php
@@ -0,0 +1,78 @@
+<!--This page is for staff-->
+<!DOCTYPE> 
+<html>
+
+<head>
+
+	<title>CCAN Management System (staff)</title>
+	<link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+<body>
+
+	<h1 class="center" style="margin-top:40px;text-align:center;width:80%">CCAN Management System (Staff)</h1>
+	
+	<div>
+		<div class="center" style="margin-top:30px;width:40%;font-size:24">
+			<a class="section" href="functions/get_food.php" target="_blank">Show Foods</a><br>
+			<a class="section" onclick="document.getElementById('pop_01').style.display='block'">Add Food</a><br>
+			<a class="section" onclick="document.getElementById('pop_02').style.display='block'">Update Food</a><br>			
+			<a class="section" href="/functions/get_orders.php" target="_blank">Show Food Orders</a><br>
+			<a class="section" href="/main.php">Visit manager page</a><br>
+		</div>
+		
+		<div id="pop_01" class="pop_up">
+			<form class="pop_up_content animate" action="/functions/add_food.php" method="post">
+				<br>
+				<label class="label">Food name</label><br>
+				<input class="input" type="text" name="name">
+				<br>
+				<label class="label">Quantity</label><br>
+				<input class="input" type="text" name="quantity">
+				<br>
+				<label class="label">Price</label><br>
+				<input class="input" type="number" name="price">
+				<br>
+				<label class="label">Zone</label><br>
+				<select style="width:100px" name="zone_id">
+					<option value="A">A</option>
+					<option value="B">B</option>
+					<option value="C">C</option>
+				</select><br>
+				<input class="apply" style="margin-right:22px" type="submit" name="submit" value="Add">
+				<a class="cancel" onclick="document.getElementById('pop_01').style.display='none'">Cancel</a>
+			</form>
+		</div>
+		
+		<div id="pop_02" class="pop_up">
+			<form class="pop_up_content animate" action="/functions/update_food.php" method="post">
+				<br>
+				<label class="label">Food name</label><br>
+				<select name="id">
+				<?php
+					include($_SERVER['DOCUMENT_ROOT'].'/connect.php');
+					$result = mysqli_query($conn, "SELECT id, name FROM food");
+					while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+							$name = $row['name'];
+							$id = $row['id'];
+							echo "<option value='$id'>" .$name."</option>";
+					}
+				?>
+				</select>
+				<br>
+				<label class="label">Quantity</label><br>
+				<input class="input" type="text" name="quantity">
+				<br>
+				<label class="label">Price</label><br>
+				<input class="input" type="number" name="price">
+				<br>
+				<input class="apply" style="margin-right:22px;padding-top:8px;padding-bottom:8px" type="submit" name="submit" value="Change">
+				<a class="cancel" onclick="document.getElementById('pop_02').style.display='none'">Cancel</a>
+			</form>
+		</div>
+	</div>
+
+</body>
+
+</html>
