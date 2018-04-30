fb7e558eb082ab07833c05de378c199718ff3565
Jackiin <38468577+Jackiin@users.noreply.github.com>
2018-04-30 16:27:16 +0800

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2018 at 10:24 AM
+-- Generation Time: Apr 30, 2018 at 10:26 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -38,6 +38,13 @@ CREATE TABLE `employee` (
   `position` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `fname`, `lname`, `salary`, `tel`, `dismissed`, `position`) VALUES
+('001', 'Yiu Chi', 'Lai', 15000, '29488888', 0, 'Chef');
+
 -- --------------------------------------------------------
 
 --
@@ -48,8 +55,20 @@ CREATE TABLE `food` (
   `id` varchar(3) NOT NULL,
   `zone_id` varchar(1) NOT NULL,
   `name` text NOT NULL,
-  `quantity` int(3) NOT NULL
+  `quantity` int(3) NOT NULL,
+  `price` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`id`, `zone_id`, `name`, `quantity`, `price`) VALUES
+('A01', 'A', 'BBQ Pork', -9, 25),
+('B01', 'B', 'Kung Pao Chicken', -23, 22),
+('B02', 'B', 'Poached Sliced Beef in Hot Chili Oil', 6, 40),
+('C01', 'C', 'Chicken with curry source', 1, 22),
+('C02', 'C', 'Instant Noodles with hams and beef', 3, 18);
 
 -- --------------------------------------------------------
 
@@ -83,8 +102,16 @@ CREATE TABLE `manage_item` (
 CREATE TABLE `orders` (
   `id` varchar(12) NOT NULL,
   `employee_id` varchar(10) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `finished` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `employee_id`, `date`, `finished`) VALUES
+('201804300002', '001', '2018-04-30', 0);
 
 -- --------------------------------------------------------
 
@@ -96,8 +123,37 @@ CREATE TABLE `orders_record` (
   `food_id` varchar(3) NOT NULL,
   `order_id` varchar(12) NOT NULL,
   `quantity` int(3) NOT NULL,
-  `order_date` date NOT NULL
+  `order_date` datetime NOT NULL,
+  `finished` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders_record`
+--
+
+INSERT INTO `orders_record` (`food_id`, `order_id`, `quantity`, `order_date`, `finished`) VALUES
+('A01', '201804300002', 1, '2018-04-30 16:06:06', 0),
+('B01', '201804300002', 2, '2018-04-30 16:06:06', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `turnover`
+--
+
+CREATE TABLE `turnover` (
+  `zone_id` varchar(1) NOT NULL,
+  `turnover` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `turnover`
+--
+
+INSERT INTO `turnover` (`zone_id`, `turnover`) VALUES
+('A', 0),
+('B', 0),
+('C', 0);
 
 -- --------------------------------------------------------
 
@@ -177,6 +233,12 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `orders_record`
   ADD PRIMARY KEY (`food_id`,`order_id`);
+
+--
+-- Indexes for table `turnover`
+--
+ALTER TABLE `turnover`
+  ADD PRIMARY KEY (`zone_id`);
 
 --
 -- Indexes for table `user`
